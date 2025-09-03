2448575bd06b2d2591a2c0eea98dfadce9a5a260
RomShemer <romshemer2000@gmail.com>
2025-09-03 23:50:58 +0300

    Add flashlight, wood door pack and Netly plugin

---
+++ UnityAngerRoom/Assets/Free Wood Door Pack/Script/Door.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DoorScript
+{
+	[RequireComponent(typeof(AudioSource))]
+
+
+	public class Door : MonoBehaviour
+	{
+		public bool open;
+		public float smooth = 1.0f;
+		float DoorOpenAngle = 90.0f;
+		float DoorCloseAngle = 0.0f;
+		public Transform fullPuzzleObject;
+		public AudioSource asource;
+		public AudioClip openDoor, closeDoor;
+		// Use this for initialization
+		void Start()
+		{
+			asource = GetComponent<AudioSource>();
+            fullPuzzleObject = GetComponent<Transform>();
+
+        }
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (open)
+			{
+				Debug.Log("open = " + open + " | current rotation: " + transform.localEulerAngles);
+				var target = Quaternion.Euler(0, DoorOpenAngle, 0);
+				transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+                fullPuzzleObject.localRotation = Quaternion.Slerp(fullPuzzleObject.localRotation, target, Time.deltaTime * 5 * smooth);
+
+
+            }
+            else
+			{
+				var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
+				transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+                if (fullPuzzleObject != null)
+                {
+                    fullPuzzleObject.localRotation = Quaternion.Slerp(fullPuzzleObject.localRotation, target1, Time.deltaTime * 5 * smooth);
+                }
+            }
+        }
+
+		public void OpenDoor()
+		{
+			Debug.Log("entering open door");
+			open = !open;
+			asource.clip = open ? openDoor : closeDoor;
+			asource.Play();
+			Debug.Log("done with open door");
+
+		}
+	}
+}
